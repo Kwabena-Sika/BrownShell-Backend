9f1fadfe79db0e3ed952396b4448a50b8d0c43ea
Kwabena-Sika <kwabenasika726@gmail.com>
2026-07-15 11:29:47 -0700

    settings changes

--- brownshell_project/settings.py
+++ brownshell_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
-    "ALLOWED_HOST",
+    "ALLOWED_HOSTS",
     default = "127.0.0.1,localhost"
 ).split(",")
 
